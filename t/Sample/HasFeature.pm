cbce9dab957405173d13ae6a54c1332fb00745e9
David Golden <xdg@xdg.me>
2016-02-29 19:10:18 -0500

    Don't check 'given' due to warnings about it

--- t/Sample/HasFeature.pm
+++ t/Sample/HasFeature.pm
@@ -5,9 +5,8 @@ use t::ToolSet::UseFeature;
 my $foo;
 my $bar;
 
-given ($foo) {
-    when (1) { $bar = "hello world" }
-    default  { $bar = "goodbye world" }
+sub hello {
+    say "hello";
 }
 
 1; # return true
